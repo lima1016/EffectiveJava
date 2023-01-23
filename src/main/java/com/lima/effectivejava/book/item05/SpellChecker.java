33945d5452a4edd7f038927bed8d7a1cd88810c0
kimyoulim <kimyoulim1016@gmail.com>
2023-01-23 16:10:06 +0900

    item 5

---
+++ src/main/java/com/lima/effectivejava/book/item05/SpellChecker.java
@@ -0,0 +1,19 @@
+package com.lima.effectivejava.book.item05;
+
+import java.util.List;
+
+public class SpellChecker {
+  // 정적 유틸리티를 잘못 하용한 예 - 유연하지 않고 테스트하기 어렵다.
+//  private static final Lexicon dictionary = "";
+
+  private SpellChecker() { // 객체 생성 방지
+
+  }
+  public static boolean isValid (String world) {
+    return false;
+  }
+
+  public static List<String> suggestions(String typo) {
+    return null;
+  }
+}
