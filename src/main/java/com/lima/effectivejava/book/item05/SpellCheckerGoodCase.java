9e9ccf6cd68875041fd0772357fa850e7023c482
kimyoulim <kimyoulim1016@gmail.com>
2023-01-26 23:19:28 +0900

    .

--- src/main/java/com/lima/effectivejava/book/item05/SpellCheckerGoodCase.java
+++ src/main/java/com/lima/effectivejava/book/item05/SpellCheckerGoodCase.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Objects;
 
 public class SpellCheckerGoodCase {
+  // 이존 객체 주입은 유연성과 테스트 용이성을 높여준다.
     private final Lexicon dictionary;
 
   private SpellCheckerGoodCase(Lexicon dictionary) {
